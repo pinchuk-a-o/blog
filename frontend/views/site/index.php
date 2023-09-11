dc3eded47535bd3b5fa7e66561c20b2a0b4e475d
Pinchuk.Alexey <pinchukao@yandex.ru>
2023-09-11 22:43:22 +0300

    init

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,51 +2,145 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = 'API документация';
 ?>
 <div class="site-index">
-    <div class="p-5 mb-4 bg-transparent rounded-3">
-        <div class="container-fluid py-5 text-center">
-            <h1 class="display-4">Congratulations!</h1>
-            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
-            <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-        </div>
-    </div>
-
     <div class="body-content">
+        <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
+                        Получить каталоги
+                    </button>
+                </h2>
+                <div id="collapse1" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Тип:</div>
+                            <div class="col-sm-3 fst-italic">GET</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Адрес:</div>
+                            <div class="col-sm-3 fst-italic">/v1/catalogs</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Тело запроса:</div>
+                            <div class="col-sm-3 fst-italic">-</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Авторизация:</div>
+                            <div class="col-sm-3 fst-italic">-</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Ответ 200:</div>
+                            <div class="col-sm-3 fst-italic">
+                                <code>
+                                    { "catalogs": [
+                                    {"id": 1, "title":"Название", "url":"/catalog/1"}, ...
+                                    ]
+                                    }
+                                </code>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-3">
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                        Получить короткие статьи каталога
+                    </button>
+                </h2>
+                <div id="collapse2" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Тип:</div>
+                            <div class="col-sm-3 fst-italic">GET</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Адрес:</div>
+                            <div class="col-sm-3 fst-italic">/v1/catalogs</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Тело запроса:</div>
+                            <div class="col-sm-3 fst-italic">-</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Авторизация:</div>
+                            <div class="col-sm-3 fst-italic">-</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Ответ 200:</div>
+                            <div class="col-sm-3 fst-italic">
+                                <code>
+                                    {
+                                    "catalogs": [
+                                    {"id": 1, "title":"Название", "url":"/catalog/1"}, ...}}
+                                    ]
+                                    }
+                                </code>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-3">
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+                        Получить полную статью
+                    </button>
+                </h2>
+                <div id="collapse3" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Тип:</div>
+                            <div class="col-sm-3 fst-italic">GET</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Адрес:</div>
+                            <div class="col-sm-3 fst-italic">/v1/catalogs</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Тело запроса:</div>
+                            <div class="col-sm-3 fst-italic">-</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Авторизация:</div>
+                            <div class="col-sm-3 fst-italic">-</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2 fw-bold">Ответ 200:</div>
+                            <div class="col-sm-3 fst-italic">
+                                <code>
+                                    {
+                                    "catalogs": [
+                                    {"id": 1, "title":"Название", "url":"/catalog/1"}, ...}}
+                                    ]
+                                    }
+                                </code>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2">
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
 </div>
