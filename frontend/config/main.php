b441e6025ef95ba1536fbbd3f6551e9c67de6cce
Pinchuk.Alexey <pinchukao@yandex.ru>
2023-09-13 16:49:21 +0300

    init

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -11,6 +11,7 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
+    'name' => 'ap1n.ru',
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
